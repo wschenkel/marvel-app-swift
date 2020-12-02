ce6d9911dc080c471700a9b52f37f4f480b660e3
Willian Schenkel <hischenkel@gmail.com>
2020-12-01 21:51:36 -0300

    change header

--- marvell-app/marvell-app/Views/Destaques/HeaderView.swift
+++ marvell-app/marvell-app/Views/Destaques/HeaderView.swift
@@ -13,48 +13,36 @@ struct HeaderView: View {
         GeometryReader { View in
             VStack {
                 VStack {
-                    Text("Alura Viagens")
-                        .font(.custom("Avenir Black", size: 20))
+                    Image("Logo")
+                    Text("Heróis na suas mãos")
+                        .font(.custom("Avenir", size: 20))
                         .foregroundColor(.white)
-                        .padding(.top, 50)
-            
-                    Text("Especial")
-                        .foregroundColor(Color.white)
-                        .font(.custom("Avenir Book", size: 20))
-                        .padding(.leading, 30)
-                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-                    Text("Brasil")
-                        .foregroundColor(Color.white)
-                        .font(.custom("Avenir Black", size: 23))
-                        .padding(.leading, 30)
-                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
                 }
-                .frame(width: View.size.width, height: 180, alignment: .top)
-                .background(Color.purple)
+                .frame(width: View.size.width, height: 130, alignment: .center)
+                .background(Color("Red"))
                 
-                
-                HStack {
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Hoteis")
-                            .font(.custom("Avenir Medium", size: 17))
-                            .foregroundColor(Color.white)
-                    }).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 50)
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 10))
-                    .background(Color.blue)
-                    .offset(x: 50)
-                    
-                    Spacer()
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        Text("Pacotes")
-                            .font(.custom("Avenir Medium", size: 17))
-                            .foregroundColor(Color.white)
-                    }).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 50)
-                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.orange, lineWidth: 10))
-                    .background(Color.orange)
-                    .offset(x: -50)
-                    
-                }.offset(y: -25)
+//                HStack {
+//                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+//                        Text("Hoteis")
+//                            .font(.custom("Avenir Medium", size: 17))
+//                            .foregroundColor(Color.white)
+//                    }).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 50)
+//                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 10))
+//                    .background(Color.blue)
+//                    .offset(x: 50)
+//
+//                    Spacer()
+//
+//                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+//                        Text("Pacotes")
+//                            .font(.custom("Avenir Medium", size: 17))
+//                            .foregroundColor(Color.white)
+//                    }).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 50)
+//                    .overlay(RoundedRectangle(cornerRadius: 10).stroke(Color.orange, lineWidth: 10))
+//                    .background(Color.orange)
+//                    .offset(x: -50)
+//
+//                }.offset(y: -25)
             }
         }
     }
